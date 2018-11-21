3ffd56312b725d9ecc6b55e42e97aa81d8aeb2cd
李铭昕 <715557344@qq.com>
2018-11-21 17:31:59 +0800

    INIT

---
+++ tests/Cases/AbstractTestCase.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Swoft.
+ *
+ * @link     https://swoft.org
+ * @document https://doc.swoft.org
+ * @contact  limingxin@swoft.org
+ * @license  https://github.com/swoft-cloud/swoft/blob/master/LICENSE
+ */
+namespace SwoftTest\Cases;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AbstractTestCase
+ */
+abstract class AbstractTestCase extends TestCase
+{
+}
